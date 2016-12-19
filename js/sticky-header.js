57dcf88e24a5ab9fe058fbe5a89a2a099519140e
Alex8320 <alex.heredia.barbosa@gmail.com>
2016-12-19 16:04:55 -0500

    Improving and sorting navbar

--- js/sticky-header.js
+++ js/sticky-header.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var stickyNavTop = $('#sticky-navbar').offset().top;
 	 
 	var stickyNavBar = function(){
@@ -11,11 +12,24 @@ $(document).ready(function(){
 	}
 
 	if (scrollTop > 540) { 
+		$("#dark-logo").show();
+		$("#light-logo").hide();
 	    $('#sticky-navbar').addClass('sticked');
 	} else {
+		$("#dark-logo").hide();
+		$("#light-logo").show();
 	    $('#sticky-navbar').removeClass('sticked'); 
 	}
 	};
+
+	if($( window ).width() > 776){
+		$("#fossasia-link").hide();
+
+	}else if($( window ).width() < 776){
+		$("#fossasia-link").show();
+		$("#light-logo").remove();
+		$("#dark-logo").remove();
+	}
 	 
 	stickyNavBar();
 	 
