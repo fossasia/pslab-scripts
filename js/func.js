6dc4f77598d42cf4fc2877175313c34b5fcf46af
Dewansh Rawat <dewanshrawat15@gmail.com>
2016-12-21 21:35:20 +0530

    Update func.js

--- js/func.js
+++ js/func.js
@@ -2,4 +2,11 @@ $(function(){
 $(".navbar-collapse ul li a").click(function(){
     $(".navbar-toggle:visible").click();
   })
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250);
+        event.preventDefault();
+    });
 })
