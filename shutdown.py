8e35fcfe4a9c924f19e51953e7ec1d1bba709550
Rebekka Rossberg <rebekka.rossberg@gmail.com>
2023-05-19 15:23:30 +0800

    fix: debugging the shutdown code

--- shutdown.py
+++ shutdown.py
@@ -9,11 +9,11 @@ The goal is to give the user a chance to avoid hard resetting the Raspi every ti
 
 PIN = 27
 gpio.setmode(gpio.BCM)
-gpio.setup(PIN, gpio.IN)
+gpio.setup(PIN, gpio.IN, gpio.PUD_UP)
 
 while True:
+    time.sleep(0.5)
     if gpio.input(PIN) == gpio.LOW:
         time.sleep(0.5) # the button needs to be long-pressed to confirm the shutdown
         if gpio.input(PIN) is gpio.LOW:
-            os.system("shutdown now-h")
-            time.sleep(1)
+            os.system("shutdown now -h")
