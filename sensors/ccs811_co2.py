81943dcc72d8547361d60697709f6595bade76d9
Rafael Lee <rafaellee.img@gmail.com>
2023-05-19 15:39:34 +0800

    Add script for CCS811 eCO2 and eTVOC sensor

--- sensors/ccs811_co2.py
+++ sensors/ccs811_co2.py
@@ -1,3 +1,31 @@
-def measure_co2(psl):
-    """ Using the CCS811 sensor to measure the ppm of CO2 in the air. """
-    return None
+#!/usr/bin/python3
+# pslab-scripts/sensors/ccs811_co2.py.py
+
+import time
+
+from pslab.external.ccs811 import CCS811
+dev = CCS811()
+time.sleep(0.05)
+
+
+print('dev.appStart()')
+dev.appStart()
+sleep_time = 1
+print(f"sleep {sleep_time} seconds")
+time.sleep(sleep_time)
+
+# time.sleep(1)
+print("dev.setMeasureMode(CCS811.MODE_CONTINUOUS)")
+dev.setMeasureMode(CCS811.MODE_CONTINUOUS)
+
+# skip the first 4 results, which are all zeros
+for i in range(4):
+    time.sleep(1)
+    dev.measure()
+
+while (1):
+    time.sleep(1)
+    ret = dev.measure()
+    status = ret['status']
+    print("eCO2 = %f, eTVOC = %f)" % (ret['eCO2'], ret['eTVOC']))
+    print(dev.decodeStatus(status))
