96791dfedb0fda890c6199bad5f17e843aa95493
Rebekka Rossberg <rebekka.rossberg@gmail.com>
2023-07-19 12:14:43 +0200

    add option for using "all" sensors

--- init_pipe.py
+++ init_pipe.py
@@ -13,7 +13,7 @@ def check_args():
     """ Ensures that the script runs with a valid experiment type parameter. """
     if len(sys.argv) > 1:
         experiment_type = sys.argv[1]
-        if experiment_type in experiment_options.keys():
+        if experiment_type in experiment_options.keys() or experiment_type == "all":
             return experiment_type
         else:
             print("Experiment type not available.")
@@ -26,18 +26,30 @@ def run_pipes(experiment_type):
     """
     Spawns one process to retrieve new data and one to export this data, both
     by sharing it via OSC, and by saving it into a CSV file.
+    
+    TODOs for the use case of connecting multiple parallel sensors:
+    - Modify the pin connections of light, o2 and temperature sensor: change the
+    input channels physically and as variables in the code, set and use additional voltage outputs.
+    - When dealing wit connection issues with the PSLab: break all measurements and
+    reconnect in a unified manner.
     """
     conn1, conn2 = multiprocessing.Pipe(duplex=False)
     
-    measuring = multiprocessing.Process(target=get_data_pslab,
-        args=(conn2, experiment_type))
-    measuring.start()
+    if experiment_type == "all":
+        measuring = [multiprocessing.Process(target=get_data_pslab,
+            args=(conn2, list(experiment_options)[i])) for i in range(4)] 
+    else:
+        measuring = [multiprocessing.Process(target=get_data_pslab,
+            args=(conn2, experiment_type))]
+    for m in measuring:
+        m.start()
     
     sharing = multiprocessing.Process(target=export,
         args=(conn1,))
     sharing.start()
     
-    measuring.join()
+    for m in measuring:
+        m.join()
     sharing.join()
     conn1.close()
     conn2.close()
